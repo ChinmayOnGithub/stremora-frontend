bf3ff31306231a67822d842d4270dc4cdd213277
Chinmay <chinmay.patil3@walchandsangli.ac.in>
2025-02-17 23:12:17 +0530

    Hamburger menu for Mobiles

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -17,7 +17,7 @@ function Home() {
       .catch((error) => {
         console.error("Error fetching videos:", error);
       });
-  }, []); // Dependency array ensures it runs only once
+  }, [videos.length]); // Dependency array ensures it runs only once
 
 
   const watchVideo = (videoId) => {
@@ -26,37 +26,26 @@ function Home() {
 
 
   return (
-    <div className="container mx-auto p-6 bg-stone-950 w-full sm:w-3/4  h-full rounded-md">
-      <h2 className="text-2xl font-bold mb-4">Total Videos: {videos.length}</h2>
+    <div className="container mx-auto p-4 sm:p-6 bg-stone-950 w-full sm:w-6/7  h-full rounded-md">
+      <h2 className="text-2xl font-bold mb-4 text-white">Total Videos: {videos.length}</h2>
 
       <div
-        className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6">
+        className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
         {videos.map((video) => (
           <div
             key={video._id}
-            className="card bg-base-200 shadow-xl hover:translate-0.5 hover:shadow-amber-50/10"
+            className="card bg-base-200 shadow-xl hover:shadow-white/10 hover:scale-[1.02] transition-all duration-200 cursor-pointer"
             onClick={() => watchVideo(video._id)} // ✅ Pass the video ID when clicked
           >
             {/* ✅ Thumbnail Image */}
-            <figure className="h-50 overflow-hidden">
+            <figure className="h-20 sm:h-50 overflow-hidden">
               <img src={video.thumbnail} alt={video.title} className="w-full h-full object-cover" />
             </figure>
 
             {/* ✅ Video Info */}
-            <div className="card-body">
+            <div className="card-body sm:w-full">
               <h3 className="card-title">{video.title}</h3>
               <p className="text-gray-500">{video.description}</p>
-
-              {/* ✅ Video Player */}
-              {/* <div className="mt-2">
-                <video src={video.videoFile} controls className="w-full rounded-md" ></video>
-              </div> */}
-
-              {/* ✅ Action Buttons */}
-              {/* <div className="card-actions justify-end mt-4"> */}
-              {/* <button className="btn btn-primary">Watch</button>
-                <button className="btn btn-outline">Save</button> */}
-              {/* </div> */}
             </div>
           </div>
         ))}
