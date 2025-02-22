1422bf6970fa76de1bc8caacad73edc8aeee8b27
Chinmay <chinmay.patil3@walchandsangli.ac.in>
2025-02-23 02:01:41 +0530

    Loading

--- src/components/Loading/Loading.jsx
+++ src/components/Loading/Loading.jsx
@@ -4,7 +4,7 @@ function Loading({ message = "Loading..." }) {
   return (
     <div className="flex flex-col justify-center items-center h-full text-gray-900 dark:text-white text-lg space-y-4">
       {/* Animated Spinner */}
-      <FaSpinner className="w-10 h-10 text-orange-500 animate-spin" />
+      <FaSpinner className="w-10 h-10 text-amber-500 animate-spin" />
 
       {/* Loading Message */}
       <h1 className="font-semibold text-xl">{message}</h1>
