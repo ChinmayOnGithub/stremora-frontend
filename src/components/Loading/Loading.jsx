466df386c4f588f4bec78aaefa3f6acd943c91e7
Chinmay <chinmay.patil3@walchandsangli.ac.in>
2025-02-23 00:47:07 +0530

    Changed loading animation again

--- src/components/Loading/Loading.jsx
+++ src/components/Loading/Loading.jsx
@@ -4,10 +4,11 @@ function Loading({ message = "Loading..." }) {
   return (
     <div className="flex flex-col justify-center items-center h-full text-gray-900 dark:text-white text-lg space-y-4">
       {/* Animated Spinner */}
-      <FaSpinner className="w-10 h-10 text-orange-500 animate-spin" />
+      {/* <FaSpinner className="w-10 h-10 text-orange-500 animate-spin" /> */}
 
       {/* Loading Message */}
       <h1 className="font-semibold text-xl">{message}</h1>
+      <img src="/loader.svg" alt="loader animation" className="" />
     </div>
   );
 }
