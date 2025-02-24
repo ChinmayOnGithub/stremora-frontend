b8e95647acc15ea434bbff402045b940c09fe48a
Chinmay <chinmay.patil3@walchandsangli.ac.in>
2025-02-24 23:38:08 +0530

    Fixed CSS errors and stopped redesigning the whole UI

--- vite.config.js
+++ vite.config.js
@@ -10,7 +10,7 @@ export default defineConfig({
     [
       react(),
       tailwindcss(),
-      daisyui()
+      daisyui
     ],
   server: {
     historyApiFallback: true, // Redirects all routes to index.html
